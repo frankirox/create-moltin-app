57154e48b3b5a714f1289323405f74261f8506ea
Jamie Barton <hi@jamiebarton.co.uk>
2018-10-12 09:00:57 +0100

    update the list of boilerplates

--- boilerplates.js
+++ boilerplates.js
@@ -1,10 +1,18 @@
 module.exports = [
   {
+    title: 'Next.js Demo Store',
+    value: 'moltin-examples/nextjs-demo-store'
+  },
+  {
     title: 'Vue.js Demo Store',
     value: 'moltin-examples/vue-demo-store'
   },
   {
-    title: 'Next.js Demo Store',
-    value: 'moltin-examples/nextjs-demo-store'
+    title: 'Swift Demo App',
+    value: 'moltin-examples/swift-demo-app'
+  },
+  {
+    title: 'React Demo Store',
+    value: 'moltin/react-demo-store'
   }
 ]
